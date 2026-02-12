e99047d358f2918faf72c980539ce7a58f751642
javanotaddshingle <3683986772@qq.com>
2026-02-12 17:10:13 +0800

    将状态栏设置为了全局透明

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,4 +48,6 @@ dependencies {
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
     implementation("androidx.cardview:cardview:1.0.0")
+    implementation("com.google.android.material:material:1.12.0")
+// 新增Material3依赖
 }
